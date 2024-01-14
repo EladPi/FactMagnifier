6c0a46dc9e59752167d7bb5b56dd9c951236d105
EladPi <127990792+EladPi@users.noreply.github.com>
2024-01-14 17:19:41 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,44 @@
+import { StyleSheet} from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './src/Redux/store';
+import { RootStackNavigator } from './src/Navigation/AppNavigator';
+import { NavigationContainer } from '@react-navigation/native';
+//import { registerForPushNotificationsAsync } from './src/Utils/registerForPushNotification';
+import { useEffect } from 'react';
+import * as Sentry from 'sentry-expo';
+
+Sentry.init({
+  dsn: 'https://114b9c1f824c5f340983d25d8af9dc85@o4506570133209088.ingest.sentry.io/4506570134454272',
+  enableInExpoDevelopment: false,
+  debug: true, // Consider setting this to false in production
+});
+
+Sentry.Native.captureException(new Error("Sentry Test Error number 2!"));
+
+
+
+export default function App() {
+
+  useEffect(() => {
+    //registerForPushNotificationsAsync();
+  }, [])
+
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <RootStackNavigator />
+      </NavigationContainer>
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
---
+++ src/Utils/registerForPushNotification.js
@@ -0,0 +1,54 @@
+import * as Device from 'expo-device';
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+
+
+export async function registerForPushNotificationsAsync() {
+  let token;
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = await Notifications.getExpoPushTokenAsync({
+      projectId: Device.modelName.expoConfig.extra.eas.projectId,
+    });
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  return await sendTokenToServer(token.data);
+}
+
+
+
+async function sendTokenToServer(token) {
+  try {
+    const response = await fetch('https://europe-west1-factsapp-cbc6a.cloudfunctions.net/registerPushToken', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ token }),
+    });
+
+    const data = await response.text();
+  } catch (error) {
+    console.error('Error on sendTokenToServer:', error);
+  }
+}
+
+
+
+
+
+/*
+      projectId: Constants.expoConfig.extra.eas.projectId,
+
+*/
