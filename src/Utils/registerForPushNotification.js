e661ea6ef71df84d72fa8bc326e40824d0a5160a
EladPi <127990792+EladPi@users.noreply.github.com>
2024-01-25 21:44:01 +0100

    Added device name

--- src/Utils/registerForPushNotification.js
+++ src/Utils/registerForPushNotification.js
@@ -4,6 +4,7 @@ import * as Notifications from 'expo-notifications';
 
 export async function registerForPushNotificationsAsync() {
   let token;
+  let deviceName;
   if (Device.isDevice) {
     const { status: existingStatus } = await Notifications.getPermissionsAsync();
     let finalStatus = existingStatus;
@@ -17,23 +18,22 @@ export async function registerForPushNotificationsAsync() {
     token = await Notifications.getExpoPushTokenAsync({
       projectId: Constants.expoConfig.extra.eas.projectId,
     });
+    deviceName= Device.deviceName;
   }
-  return await sendTokenToServer(token.data);
+  return await sendTokenToServer(token.data, deviceName);
 }
 
 
 
-async function sendTokenToServer(token) {
+async function sendTokenToServer(token, deviceName) {
   try {
     const response = await fetch('https://europe-west1-factsapp-cbc6a.cloudfunctions.net/registerPushToken', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ token }),
+      body: JSON.stringify({ token, deviceName }),
     });
-
-    const data = await response.text();
   } catch (error) {
     console.error('Error on sendTokenToServer:', error);
   }
